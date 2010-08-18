7a64c4acfa3817bf476fa190e64919921762401e
Yoichi Hirai <yh@is.s.u-tokyo.ac.jp>
2010-08-18 14:31:26 +0900

    proved the -1 \le difference \le 1 relation.

--- Data/BST/Internal.v
+++ Data/BST/Internal.v
@@ -85,13 +85,196 @@ Fixpoint lpos (n: positive): nat :=
     | xI n' => S (lpos n')
   end.
       
-Definition l: N -> nat :=
+Definition lN: N -> nat :=
   fun n =>
     match n with
       | N0 => O
       | Npos p => lpos p
     end.
 
+Definition l: Map -> nat :=
+  fun t => lN (size t).
 
-(* and operation ! *)
- 
+Fixpoint near (m n: nat): bool :=
+  match m, n with
+    | O, O => true
+    | O, S O => true
+    | S O, O => true
+    | S m', S n' => near m' n'
+    | _, _ => false
+  end.
+
+Lemma near_ind:
+  forall (m n: nat),
+    near (S m) (S n) = near m n.
+  intros m n.
+  simpl.
+  case m.
+  reflexivity.
+  intro n0.
+  reflexivity.
+  Qed.
+
+Definition isBalanced: Map -> Map -> bool :=
+fun a b =>
+  near (l a) (l b).
+
+Lemma near_plus_minus:
+  forall m n: nat,
+    near m n = true <-> m = n \/ m = S n \/ n = S m.
+induction m.
+intro n.
+split.
+compute.
+case n.
+intro irr.
+left.
+reflexivity.
+clear n.
+intro n.
+case n.
+intro irr.
+right.
+right.
+reflexivity.
+intro n0.
+intro f.
+discriminate.
+compute.
+case n.
+intro.
+reflexivity.
+clear n.
+intro n.
+intro disj.
+case disj.
+intro f.
+discriminate.
+clear disj.
+intro disj.
+case disj.
+intro f.
+discriminate.
+intro f.
+assert (n=O).
+injection f.
+auto.
+rewrite H.
+reflexivity.
+intro n.
+case n.
+split.
+compute.
+case m.
+intro irr.
+right.
+left.
+reflexivity.
+clear n.
+intro n.
+intro.
+discriminate.
+compute.
+case m.
+intro.
+reflexivity.
+clear n.
+intro n.
+intro disj.
+elim disj.
+intro f.
+discriminate.
+clear disj.
+intro disj.
+case disj.
+clear disj.
+intro f.
+discriminate.
+intro f.
+discriminate.
+clear n.
+intro n.
+split.
+intro ne.
+assert (near m n = true).
+inversion ne.
+case m.
+reflexivity.
+intro n0.
+reflexivity.
+assert (m = n \/ m = (S n) \/ n = (S m)).
+elim IHm with n.
+clear IHm.
+intro ih.
+intro irr.
+clear irr.
+apply ih.
+exact H.
+case H0.
+intro mn.
+left.
+clear H0 H ne IHm a k.
+rewrite mn.
+reflexivity.
+intro msn.
+clear H0 H ne IHm a k.
+case msn.
+clear msn.
+intro msn.
+right.
+left.
+rewrite msn.
+reflexivity.
+intro nsm.
+right.
+right.
+rewrite nsm.
+reflexivity.
+intro disj.
+elim disj.
+clear disj.
+intro smsn.
+assert (near m n = true).
+elim IHm with n.
+intro irr.
+intro ih.
+apply ih.
+left.
+injection smsn.
+auto.
+rewrite near_ind.
+exact H.
+clear disj.
+intro disj.
+case disj.
+clear disj.
+intro smssn.
+rewrite near_ind.
+assert (m=S n).
+injection smssn.
+auto.
+elim IHm with n.
+intro irr.
+clear irr.
+intro app.
+apply app.
+right.
+left.
+exact H.
+clear disj.
+intro snssm.
+assert (n = S m).
+injection snssm.
+auto.
+rewrite near_ind.
+elim IHm with n.
+intro irr.
+clear irr.
+intro app.
+apply app.
+right.
+right.
+exact H.
+Qed.
+
+
