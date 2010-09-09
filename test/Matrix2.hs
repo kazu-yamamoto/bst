02b57412d7c7476b5fff853a2165a603d2635d82
Kazu Yamamoto <kazu@iij.ad.jp>
2010-09-09 15:57:06 +0900

    test2.

---
+++ test/Matrix2.hs
@@ -0,0 +1,38 @@
+module Matrix where
+
+import System.Exit
+import System.Process
+
+main :: IO ()
+main = do
+    results <- mapM test [(x,y) | x <- [20..50], y <- [100,105..215]] -- FIXME
+    let res = (concatMap toString . map toRes . filter ok $ results)
+           ++ "\n"
+           ++ (concatMap toString . map toRes . filter ng $ results)
+    writeFile "RES" res
+  where
+    toString (x,y) = show x ++ " " ++ show y ++ "\n"
+    toRes :: (Bool,(Int,Int)) -> (Float,Float)
+    toRes (_,(x,y)) = (fromIntegral x / 10, fromIntegral y / 100)
+    ok = fst
+    ng = not . fst
+
+test :: (Int, Int) -> IO (Bool,(Int, Int))
+test (n,m) = do
+  system $ delta n ++ " " ++ from ++ " | " ++ ratio m ++ " > " ++ to
+  status <- system "runghc -i.. -DMETHOD=3 -DTEST Test2.hs" -- FIXME
+  case status of
+      ExitSuccess -> return (True,(n,m))
+      _           -> return (False,(n,m))
+    
+delta :: Int -> String
+delta n = "sed -e 's/^deltaU = [0-9][0-9]/deltaU = " ++ show n ++  "/'"
+
+ratio :: Int -> String
+ratio n = "sed -e 's/^ratioU = [0-9][0-9][0-9]/ratioU = " ++ show n ++  "/'"
+
+from :: String
+from = "../Data/SMap/Balance-test.hs"
+
+to :: String
+to = "../Data/SMap/Balance.hs"
---
+++ test/Test2.hs
@@ -0,0 +1,141 @@
+ module Main where
+
+import Data.Maybe
+import Control.Monad
+import Data.SMap.Types
+import Data.SMap.Balance
+import Data.SMap.Internal
+import Test.Framework (defaultMain, testGroup, Test)
+import Test.Framework.Providers.HUnit
+import Test.HUnit hiding (Test, Testable)
+import Prelude as P
+
+tests :: [Test]
+tests = [ testGroup "Test Case" [
+               testCase "upper" test_upper
+             , testCase "right" test_right
+--             , testCase "lower" test_lower
+             , testCase "left" test_left
+             ]
+        ]
+        
+main :: IO ()
+main = defaultMain tests
+
+----------------------------------------------------------------
+
+test_upper :: Assertion
+test_upper = do
+    putStrLn $ "(" ++ show deltaU ++ "," ++ show ratioU ++ ")"
+    if deltaU < 25
+        then True @?= True
+        else do
+          unless (valid t) (error "test_upper")
+          valid (deleteMin t) @?= True
+  where
+    sizeX = (deltaU `div` 10) - 1
+    x = makeTree sizeX 10
+    sizeY = ((deltaU - 5) `div` 10) - 1
+    y = makeTree sizeY 101
+    t = nd 2 (sg 1) (nd 100 (nd 3 Tip x) y)
+
+----------------------------------------------------------------
+
+test_right :: Assertion
+test_right = if deltaU < 45
+             then True @?= True
+             else do
+                 unless (valid t) (error "test_right")
+                 valid (deleteMin t) @?= True
+  where
+    sizeX = (deltaU `div` 5) - 5
+    x = makeTree sizeX 10
+    t = nd 2 (sg 1) (nd 1000 (nd 100 x (sg 101)) (sg 1001))
+
+----------------------------------------------------------------
+
+test_lower :: Assertion
+test_lower = do
+    putStrLn $ "(" ++ show deltaU ++ "," ++ show ratioU ++ ")"
+    print (se,sz,sw,sy)
+    putStrLn $ showTree t
+    unless (valid t) (error "test_lower")
+    let t' = deleteMin t
+    putStrLn $ showTree t'
+    valid t' @?= True
+  where
+    (se,sz,sw,sy) = findLow
+    e = makeTree se 0
+    z = makeTree sz 2000
+    w = makeTree sw 4000
+    y = makeTree sy 6000
+    t = nd 1000 e (nd 5000 (nd 3000 z w) y)
+
+findLow :: (Int,Int,Int,Int)
+findLow = fromJust . head . P.filter isJust . P.map findLow' $ [1..]
+
+findLow' :: Int -> Maybe (Int,Int,Int,Int)
+findLow' d = if isBal x y && isBal y x && isBal z w && isBal w z
+             then Just (e,z,w,y)
+             else Nothing
+  where
+    x = z + w + 1
+    y = d + 1
+    z = d
+    w = ceiling $ (fromIntegral (d + 2)) * r - fromIntegral d - 4
+    r :: Float
+    r = fromIntegral ratioU / fromIntegral ratioD
+    invd :: Float
+    invd = fromIntegral deltaD / fromIntegral deltaU
+    e = (ceiling $ fromIntegral (x + y + 2) * invd) - 1
+
+----------------------------------------------------------------
+
+test_left :: Assertion
+test_left = if deltaU >= 33
+             then True @?= True
+             else do
+                 unless (valid t) (error "test_left")
+                 valid (deleteMin t) @?= True
+  where
+    (sd,sx,sy) = findHigh
+    d = makeTree sd 0
+    x = makeTree sx 2000
+    y = makeTree sy 4000
+    t = (nd 1000 d (nd 3000 x y))
+
+findHigh :: (Int,Int,Int)
+findHigh = fromJust . head . P.filter isJust . P.map findHigh' $ [1..]
+
+findHigh' :: Int -> Maybe (Int,Int,Int)
+findHigh' x = if isBal d e && isBal e d && isBal x y && isBal y x
+           then Just (d,x,y)
+           else Nothing
+  where
+    y = floor $ (fromIntegral (x + 1)) * invr
+    d = floor $ (fromIntegral (x + y + 2)) * invd
+    e = x + y + 1
+    invr :: Float
+    invr = fromIntegral ratioD / fromIntegral ratioU
+    invd :: Float
+    invd = fromIntegral deltaD / fromIntegral deltaU
+
+----------------------------------------------------------------
+
+makeTree :: Size -> Int -> Map Int ()
+makeTree siz low = fromList . take siz . zip [low..] $ (repeat ())
+
+nd :: Int -> Map Int () -> Map Int () -> Map Int ()
+nd k l r = Bin sz k () l r
+  where
+    sz = size l + size r + 1
+
+sg :: Int -> Map Int ()
+sg k = singleton k ()
+
+isBal :: Int -> Int -> Bool
+isBal a b = deltaU * x >= deltaD * y
+  where
+    x = a + 1
+    y = b + 1
+
