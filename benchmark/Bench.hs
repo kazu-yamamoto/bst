544e46e2ead20debd2653a2994812771f9d187ce
Kazu Yamamoto <kazu@iij.ad.jp>
2010-10-05 23:41:36 +0900

    improving bench for insert.

--- benchmark/Bench.hs
+++ benchmark/Bench.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Random
 import Data.SMap.Types
 import Data.SMap.Internal
 import Progression.Main
@@ -7,15 +8,26 @@ import Criterion.Main (bench, bgroup, whnf)
 
 main :: IO ()
 main = do
-    let !t1 = fromList $ zip [1..1000] (repeat ()) :: Map Int ()
-        !t2 = fromList $ zip [1..10000] (repeat ()) :: Map Int ()
-        !t3 = fromList $ zip [1..100000] (repeat ()) :: Map Int ()
+    let !l1 = zip [1..1000]   (repeat ()) :: [(Int,())]
+        !l2 = zip [1..10000]  (repeat ()) :: [(Int,())]
+        !l3 = zip [1..100000] (repeat ()) :: [(Int,())]
+        !r1 = zip (take   1000 . randoms . mkStdGen $ 12345) (repeat ()) :: [(Int,())]
+        !r2 = zip (take  10000 . randoms . mkStdGen $ 12345) (repeat ()) :: [(Int,())]
+        !r3 = zip (take 100000 . randoms . mkStdGen $ 12345) (repeat ()) :: [(Int,())]
+        !t1 = fromList l1
+        !t2 = fromList l2
+        !t3 = fromList l3
     defaultMain $
         bgroup "" [
-             bench "ins 1000"   $ whnf (insert 1001 ()) t1
-           , bench "ins 10000"  $ whnf (insert 10001 ()) t2
-           , bench "ins 100000" $ whnf (insert 100001 ()) t3
-           , bench "del 1000"   $ whnf (delete 500) t1
-           , bench "del 10000"  $ whnf (delete 5000) t2
-           , bench "del 100000" $ whnf (delete 50000) t3
+             bench "ins i1000"   $ whnf fromList l1
+           , bench "ins i10000"  $ whnf fromList l2
+           , bench "ins i100000" $ whnf fromList l3
+           , bench "ins r1000"   $ whnf fromList r1
+           , bench "ins r10000"  $ whnf fromList r2
+           , bench "ins r100000" $ whnf fromList r3
+{-
+           , bench "del 1000"   $ whnf (delete 502) t1
+           , bench "del 10000"  $ whnf (delete 5002) t2
+           , bench "del 100000" $ whnf (delete 50002) t3
+-}
            ]
