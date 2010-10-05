c192bba4229aae57c44ab71cefdff2e2dda9a2f9
Kazu Yamamoto <kazu@iij.ad.jp>
2010-10-05 18:02:24 +0900

    -O2 for compile.

--- benchmark/Bench.hs
+++ benchmark/Bench.hs
@@ -1,20 +1,21 @@
 module Main where
 
 import Data.SMap.Types
-import Data.SMap.Balance
 import Data.SMap.Internal
 import Progression.Main
 import Criterion.Main (bench, bgroup, whnf)
 
 main :: IO ()
 main = do
-    let !t1 = fromList $ zip [1..1000] (repeat ())
-        !t2 = fromList $ zip [1..10000] (repeat ())
-        !t3 = fromList $ zip [1..100000] (repeat ())
+    let !t1 = fromList $ zip [1..1000] (repeat ()) :: Map Int ()
+        !t2 = fromList $ zip [1..10000] (repeat ()) :: Map Int ()
+        !t3 = fromList $ zip [1..100000] (repeat ()) :: Map Int ()
     defaultMain $
-        bgroup "insert" [ 
-             bench "1000" $ whnf (insert 1001 ()) t1
-           , bench "10000" $ whnf (insert 10001 ()) t2
-           , bench "100000" $ whnf (insert 100001 ()) t3
+        bgroup "" [
+             bench "ins 1000"   $ whnf (insert 1001 ()) t1
+           , bench "ins 10000"  $ whnf (insert 10001 ()) t2
+           , bench "ins 100000" $ whnf (insert 100001 ()) t3
+           , bench "del 1000"   $ whnf (delete 500) t1
+           , bench "del 10000"  $ whnf (delete 5000) t2
+           , bench "del 100000" $ whnf (delete 50000) t3
            ]
-
--- benchmark/bench.sh
+++ benchmark/bench.sh
@@ -4,14 +4,14 @@ make distclean
 
 make clean
 make METHOD=3
-./Bench --compare= --name=adams42
+./Bench -m run --compare= --name=adams42
 
 make clean
 make METHOD=4
-./Bench --compare= --name=adams32
+./Bench -m run --compare= --name=adams32
 
 make clean
 make METHOD=5
-./Bench --compare= --name=nr32
+./Bench -m run --compare= --name=nr32
 
 ./Bench -m graph --compare=nr32,adams32,adams42
