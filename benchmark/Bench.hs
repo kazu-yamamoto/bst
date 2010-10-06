9fa0677111c035dba2c653be0c7d5398746c47b5
Kazu Yamamoto <kazu@iij.ad.jp>
2010-10-06 14:14:32 +0900

    improving.

--- benchmark/Bench.hs
+++ benchmark/Bench.hs
@@ -17,24 +17,32 @@ main = do
         !x1 = zip l1 (repeat ())
         !x2 = zip l2 (repeat ())
         !x3 = zip l3 (repeat ())
-        !r1 = zip (take   1000 . randoms . mkStdGen $ seed) (repeat ()) :: [(Int,())]
-        !r2 = zip (take  10000 . randoms . mkStdGen $ seed) (repeat ()) :: [(Int,())]
-        !r3 = zip (take 100000 . randoms . mkStdGen $ seed) (repeat ()) :: [(Int,())]
         !t1 = fromList x1
         !t2 = fromList x2
         !t3 = fromList x3
+        !r1 = (take   1000 . randoms . mkStdGen $ seed) :: [Int]
+        !r2 = (take  10000 . randoms . mkStdGen $ seed) :: [Int]
+        !r3 = (take 100000 . randoms . mkStdGen $ seed) :: [Int]
+        !y1 = zip r1 (repeat ())
+        !y2 = zip r2 (repeat ())
+        !y3 = zip r3 (repeat ())
+        !s1 = fromList y1
+        !s2 = fromList y2
+        !s3 = fromList y3
     defaultMain $
         bgroup "" [
              bench "ins i1000"   $ nf fromList x1
            , bench "ins i10000"  $ nf fromList x2
            , bench "ins i100000" $ nf fromList x3
-           , bench "ins r1000"   $ nf fromList r1
-           , bench "ins r10000"  $ nf fromList r2
-           , bench "ins r100000" $ nf fromList r3
+           , bench "ins r1000"   $ nf fromList y1
+           , bench "ins r10000"  $ nf fromList y2
+           , bench "ins r100000" $ nf fromList y3
            , bench "del i1000"   $ nf (map (del t1)) l1
-           , bench "del i10000"  $ nf (map (del t2)) l2
-           , bench "del i100000" $ nf (map (del t3)) l3
-
+           , bench "del i10000"  $ nf (map (del t2)) (skip l2 400)
+           , bench "del i100000" $ nf (map (del t3)) (skip l3 8000)
+           , bench "del r1000"   $ nf (map (del s1)) r1
+           , bench "del r10000"  $ nf (map (del s2)) (skip r2 400)
+           , bench "del r100000" $ nf (map (del s3)) (skip r3 8000)
            ]
 
 instance (NFData k, NFData a) => NFData (Data.SMap.Map k a) where
@@ -42,3 +50,11 @@ instance (NFData k, NFData a) => NFData (Data.SMap.Map k a) where
 
 del :: Map Int () -> Int -> Map Int ()
 del = flip delete
+
+skip :: [a] -> Int -> [a]
+skip xs n = skip' xs 0
+  where
+    skip' [] _ = []
+    skip' (y:ys) c
+      | c `mod` n == 0 = y : skip' ys (c+1)
+      | otherwise      =     skip' ys (c+1)
