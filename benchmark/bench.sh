604000e9bd86eb6972a4745f84b6839828dcc19b
Kazu Yamamoto <kazu@iij.ad.jp>
2010-10-08 14:19:09 +0900

    sleep well!.

--- benchmark/bench.sh
+++ benchmark/bench.sh
@@ -4,14 +4,17 @@ make distclean
 
 make clean
 make METHOD=3
+sleep 60
 ./Bench -m run --compare= --name=adams42
 
 make clean
 make METHOD=4
+sleep 60
 ./Bench -m run --compare= --name=adams32
 
 make clean
 make METHOD=5
+sleep 60
 ./Bench -m run --compare= --name=nr32
 
 ./Bench -m graph --compare=nr32,adams32,adams42
