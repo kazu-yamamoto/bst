9d69be53b7d6401a1040de756e24e73a1e1bd8db
Kazu Yamamoto <kazu@iij.ad.jp>
2011-01-26 13:37:27 +0900

    measuring 10 points.

--- benchmark-rational/insert/bench.sh
+++ benchmark-rational/insert/bench.sh
@@ -5,33 +5,41 @@ make distclean
 make clean
 make METHOD=5
 sleep 60
-./Bench -m run --compare= --name=original3-2
+./Bench -m run --compare= --name=c
 
 make clean
 make METHOD=6
 sleep 60
-./Bench -m run --compare= --name=original4-54
+./Bench -m run --compare= --name=a
 
 make clean
 make METHOD=7
 sleep 60
-./Bench -m run --compare= --name=original92-119
+./Bench -m run --compare= --name=b
 
 make clean
 make METHOD=9
 sleep 60
-./Bench -m run --compare= --name=original92-53
+./Bench -m run --compare= --name=d
 
 make clean
 make METHOD=9
 sleep 60
-./Bench -m run --compare= --name=original52-32
+./Bench -m run --compare= --name=f
 
 make clean
 make METHOD=10
 sleep 60
-./Bench -m run --compare= --name=original52-75
+./Bench -m run --compare= --name=h
 
-./Bench -m graph --compare=original3-2,original4-54,original92-119,original92-53,original52-32,original52-75
+make clean
+make METHOD=11
+sleep 60
+./Bench -m run --compare= --name=g
 
-sh plot.sh
+make clean
+make METHOD=12
+sleep 60
+./Bench -m run --compare= --name=e
+
+./Bench -m graph  --plot=insert.png --compare=a,b,c,d,e,f,g,h
